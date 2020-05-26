b7d5af99b40ea373ab020c11cef9e2b357daaf7e
Ewe Lin Loo <ewelinloo@gmail.com>
2020-05-27 01:51:55 +1000

    Sample text now in default selected size

--- script.js
+++ script.js
@@ -13,51 +13,58 @@ const fontInfo = [
   { fontFamily: 'Roboto', designer: 'Christian Robertson' },
   { fontFamily: 'Roboto Mono', designer: 'Christian Robertson' },
   { fontFamily: 'Roboto Slab', designer: 'Christian Robertson' },
-];
+]
 
 // Creating font cards
-const main = document.querySelector('main');
+const main = document.querySelector('main')
+const selectedSize = document.getElementById('font-size')
 
 for (let i = 0; i < fontInfo.length; i++) {
-  let info = document.createTextNode(`${fontInfo[i].fontFamily}`);
-  const div = document.createElement('div');
-  let p = document.createElement('p');
-  p.appendChild(info);
-  div.appendChild(p);
+  let info = document.createTextNode(`${fontInfo[i].fontFamily}`)
+  const div = document.createElement('div')
+  let p = document.createElement('p')
+  p.appendChild(info)
+  div.appendChild(p)
 
-  info = document.createTextNode(`${fontInfo[i].designer}`);
-  p = document.createElement('p');
-  p.appendChild(info);
-  div.appendChild(p);
+  info = document.createTextNode(`${fontInfo[i].designer}`)
+  p = document.createElement('p')
+  p.appendChild(info)
+  div.appendChild(p)
 
-  const icon = document.createElement('i');
-  const iconClass = document.createAttribute('class');
-  iconClass.value = 'fas fa-plus';
-  icon.setAttributeNode(iconClass);
-  div.appendChild(icon);
+  const icon = document.createElement('i')
+  const iconClass = document.createAttribute('class')
+  iconClass.value = 'fas fa-plus'
+  icon.setAttributeNode(iconClass)
+  div.appendChild(icon)
 
-  sampleText = 'Then came the night of the first falling star.';
-  info = document.createTextNode(`${sampleText}`);
-  p = document.createElement('p');
-  p.appendChild(info);
-  let sampleStyle = document.createAttribute('style');
-  sampleStyle.value = `font-family: ${fontInfo[i].fontFamily}`;
-  p.setAttributeNode(sampleStyle);
-  div.appendChild(p);
+  sampleText = 'Then came the night of the first falling star.'
+  info = document.createTextNode(`${sampleText}`)
+  p = document.createElement('p')
+  p.appendChild(info)
+  let sampleStyle = document.createAttribute('style')
+  sampleStyle.value = `font-family: ${fontInfo[i].fontFamily}; font-size: ${selectedSize.value}px`
+  p.setAttributeNode(sampleStyle)
+  console.log(p)
+  div.appendChild(p)
 
-  main.appendChild(div);
+  main.appendChild(div)
 }
 
 // Changes sample text to provided input
-const selectedInput = document.getElementById('sampleInput');
+const selectedInput = document.getElementById('sampleInput')
 selectedInput.addEventListener('input', (event) => {
   if (selectedInput.value === 'Type something' || selectedInput.value === '') {
-    sampleText = 'Then came the night of the first falling star.';
+    sampleText = 'Then came the night of the first falling star.'
   } else {
-    sampleText = event.target.value;
+    sampleText = event.target.value
   }
-  sampleP = document.querySelectorAll('p:nth-child(4)');
+  sampleP = document.querySelectorAll('p:nth-child(4)')
   for (let i = 0; i < sampleP.length; i++) {
-    sampleP[i].textContent = sampleText;
+    sampleP[i].textContent = sampleText
   }
-});
+})
+
+// Changes sample text to selected size
+selectedSize.addEventListener('change', (event) => {
+  console.log('SELECTEDSIZE: ', selectedSize.value)
+})
